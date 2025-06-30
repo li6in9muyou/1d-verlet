6570d8b9ce93f3f1f072924fa7256055d75d75d2
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-06-30 20:46:05 +0800

    wip: bounded movement

--- verlet-sketch.js
+++ verlet-sketch.js
@@ -7,25 +7,41 @@ const MAX_Y = 600;
 const HALF_SIZE = 6;
 
 const a = {
-  prevY: 504,
+  prevY: 508,
   y: 500,
   acc: 0,
   m: 10,
+  get v() {
+    return a.y - a.prevY;
+  },
 };
 
 const dt = 1;
 
 function doDt(elapsed) {
-  const va = a.y - a.prevY;
-  const nextY = a.y + va * elapsed + a.acc * elapsed * elapsed;
+  const nextY = a.y + a.v * elapsed + a.acc * elapsed * elapsed;
   a.prevY = a.y;
   a.y = nextY;
+}
+
+function doBounds() {
+  if (a.y < 0 + HALF_SIZE) {
+    a.prevY = a.y;
+    a.y = 0 + HALF_SIZE;
+  }
+  if (a.y > MAX_Y - HALF_SIZE) {
+    a.prevY = a.y;
+    a.y = MAX_Y - HALF_SIZE;
+  }
 }
 
 function draw() {
   background("#444");
 
   doDt(dt);
+  console.log("libq draw/v after dodt", a.v);
+  doBounds();
+  console.log("libq draw/v after bounds", a.v);
 
   renderBox(a.y, "green");
   text(`a=${a.y - a.prevY}`, 4, 30);
