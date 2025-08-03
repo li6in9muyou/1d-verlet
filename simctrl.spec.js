26098cf3e70927be726ae510528d9c45e35d7579
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-03 22:39:39 +0800

    wip: add tests

--- simctrl.spec.js
+++ simctrl.spec.js
@@ -33,5 +33,78 @@ describe("simctrl", () => {
     s.requestPrevStep();
     expect(s.shouldSetState()).toBe(true);
     expect(s.getState()).toBe(102);
+
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(101);
+
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(103);
+  });
+
+  test("basic prev 2", () => {
+    s.onStep(100);
+    s.onStep(101);
+    s.onStep(102);
+    s.onStep(103);
+
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(102);
+
+    s.nextStep();
+    s.onStep(204);
+    s.nextStep();
+    s.onStep(205);
+    s.nextStep();
+    s.onStep(206);
+
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(205);
+
+    s.requestPrevStep();
+    s.requestPrevStep();
+    s.requestPrevStep();
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(204);
+
+    s.nextStep();
+    s.onStep(305);
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(204);
+
+    s.nextStep();
+    s.onStep(305);
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(204);
+
+    s.nextStep();
+    s.onStep(305);
+    s.requestPrevStep();
+    expect(s.shouldSetState()).toBe(true);
+    expect(s.getState()).toBe(204);
+  });
+
+  test("basic prev 3", () => {
+    s.onStep(100);
+    s.onStep(101);
+    s.onStep(102);
+    s.onStep(103);
+
+    s.requestPrevStep();
+    s.requestPrevStep();
+    expect(s.getState()).toBe(101);
+
+    s.nextStep();
+    s.onStep(12);
+    s.nextStep();
+    s.onStep(13);
+    s.requestPrevStep();
+    expect(s.getState()).toBe(12);
   });
 });
