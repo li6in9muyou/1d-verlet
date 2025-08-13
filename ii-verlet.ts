a79b51dbbec2a1348df854eccd40cccfa50c5cb0
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-13 22:38:15 +0800

    wip: tweak demo

--- ii-verlet.ts
+++ ii-verlet.ts
@@ -11,6 +11,7 @@ let WORLD: World = {
   springs: [
     { one: 1, two: 0, k: 4, restingLen: 100 },
     { one: MIN_Y_ANCHOR, two: 0, k: 40, restingLen: 80 },
+    { one: 2, two: MAX_Y_ANCHOR, k: 1, restingLen: 80 },
   ],
   gravityAcc: 0.1001344,
   dragCoeff: 0,
@@ -22,11 +23,16 @@ let WORLD: World = {
   SPRING_TENSION_OFFSET: 3,
   SPRING_MARGIN_X: -7,
   dt: 1 / 3000,
-  boxes: [yv(100, 0, 0), yv(200, 0, 0)],
-  sizes: [24, 24, 12, 12 / 5, 24],
+  boxes: [
+    yv(100, 0, 0),
+    yv(200, 0, 0),
+    yv(600 - 80, 0, 0),
+    yv(600 - 80 - 60, 0, 0),
+  ],
+  sizes: [24, 24, 12, 12, 24],
   colors: ["red", "green", "#0ff", "#ff0", "#f0f"],
   names: ["a", "b", "c", "d", "e"],
-  masses: [3000, 3000, 50, 10, 100],
+  masses: [3000, 3000, 50, 50, 100],
   statNextLineY: 0,
   ctrl: {
     history: { MAX_STATES: 50, cursor: 0, states: [] },
