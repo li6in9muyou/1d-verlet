c60b83794573e928bec7cc186a28f5f77700303c
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-19 22:00:39 +0800

    chore: use jekyll to insert cdn p5js links

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ export default defineConfig({
   server: {
     port: 13333,
   },
+  plugins: [
+    {
+      name: "remove-jekyll-templates",
+      transformIndexHtml(html) {
+        const regex = /{% if site\.dev %}|{% else %}[\s\S]*?{% endif %}/g;
+        return html.replace(regex, "");
+      },
+    },
+  ],
 });
