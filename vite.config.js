96883f8dd716611e2d439840cc055d1fe8f26f51
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-25 16:19:12 +0800

    ok: cache vendor stuff

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,23 @@ export default defineConfig({
         return html.replace(regex, "");
       },
     },
+    // 添加一个新的插件来处理 vendor 目录的缓存标头
+    {
+      name: "add-cache-control-for-vendor",
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          // Vite 会将 public 目录映射到根路径 /
+          // 所以 public/vendor/some-file.js 的访问路径是 /vendor/some-file.js
+          if (req.url.startsWith("/vendor/")) {
+            res.setHeader(
+              "Cache-Control",
+              "public, max-age=31536000, immutable",
+            );
+          }
+          // 将请求传递给下一个中间件
+          next();
+        });
+      },
+    },
   ],
 });
